0fc5d6ffa71c75e57efa71ad39e75a3fad143a84
saucekode <saucekoder@gmail.com>
2021-12-30 12:17:28 -0800

    fixed redirect

--- src/appconstants/index.js
+++ src/appconstants/index.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = "https://fotosplash.herokuapp.com";
-export const OAUTH2_REDIRECT_URI = "http://objective-dubinsky-cb9c46.netlify.app"
+export const OAUTH2_REDIRECT_URI = "https://objective-dubinsky-cb9c46.netlify.app"
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
